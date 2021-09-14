feab883ed79a67ccdd47c03b50ce4643c259a121
Michael Arntzenius <daekharel@gmail.com>
2021-09-14 18:11:09 +0200

    implement GUI & clearing, prefix all commands with "clip"

--- indexed_clipboard.py
+++ indexed_clipboard.py
@@ -1,4 +1,4 @@
-from talon import Module, Context, actions, clip
+from talon import Module, Context, actions, clip, imgui
 
 mod = Module()
 ctx = Context()
@@ -8,6 +8,15 @@ mod.list('clipboard_index', desc="Names of indices into the indexed clipboard.")
 
 def update_index_names():
     ctx.lists['self.clipboard_index'] = list(indexed_clipboard.keys())
+
+@imgui.open()
+def gui(gui: imgui.GUI):
+    gui.text("Indexed clipboard")
+    gui.line()
+    for index, value in indexed_clipboard.items():
+        gui.text(f'{index}: {value}'
+                 if len(value) <= 20 else
+                 f'{index}: {value[:20]}...')
 
 @mod.action_class
 class ModuleActions:
@@ -19,3 +28,13 @@ class ModuleActions:
     def indexed_clipboard_paste(index: str):
         """Pastes from the given index of the indexed clipboard."""
         actions.user.paste(indexed_clipboard[index])
+
+    def indexed_clipboard_clear(index: str):
+        """Clears the given index from the indexed clipboard."""
+        del indexed_clipboard[index]
+        updated_next_names()
+
+    def indexed_clipboard_toggle():
+        """Views the keys of the indexed clipboard."""
+        if gui.showing: gui.hide()
+        else: gui.show()
--- indexed_clipboard.talon
+++ indexed_clipboard.talon
@@ -1,2 +1,4 @@
-clip <user.word>: user.indexed_clipboard_copy(word)
-paste {user.clipboard_index}: user.indexed_clipboard_paste(clipboard_index)
+clip copy <user.word>: user.indexed_clipboard_copy(word)
+clip paste {user.clipboard_index}: user.indexed_clipboard_paste(clipboard_index)
+clip clear {user.clipboard_index}: user.indexed_clipboard_clear(clipboard_index)
+clip list: user.indexed_clipboard_toggle()
